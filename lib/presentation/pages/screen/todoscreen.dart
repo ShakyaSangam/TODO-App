08c613b9cd2b0f3cfd331d682bdfd1111310f6e2
Sangam Shakya <sangam.hikre@gmail.com>
2021-04-20 10:46:08 +0545

    todo screen optimized

---
+++ lib/presentation/pages/screen/todoscreen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:todo/presentation/widgets/task_btn_widget.dart';
+import 'package:todo/presentation/widgets/task_list_widget.dart';
+
+class TodoScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlue,
+      body: SafeArea(
+        child: ListView(
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                TaskBtnWidget(),
+                Container(
+                  padding: EdgeInsets.all(20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        "TO-DO",
+                        style: TextStyle(color: Colors.white, fontSize: 25),
+                      ),
+                      SizedBox(width: 14),
+                      Text(
+                        "List",
+                        style: TextStyle(color: Colors.white70, fontSize: 18),
+                      ),
+                    ],
+                  ),
+                ),
+                TaskList(),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
